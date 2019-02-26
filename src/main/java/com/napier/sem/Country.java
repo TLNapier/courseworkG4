75dac07c99c8c4c7dd4dea7afd3b8036cd791132
Ahad Malik <ahadm94@hotmail.co.uk>
2019-02-26 15:43:44 +0000

    Country class added

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,69 @@
+package com.napier.sem;
+
+public class Country {
+
+
+
+    private String Code;
+    private String Name;
+    private String Continent;
+    private String Region;
+    private String Population;
+    private String Capital;
+
+
+
+        public String getCode() {
+            return Code;
+        }
+
+        public void setCode(String code) {
+            Code = code;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        public String getContinent() {
+            return Continent;
+        }
+
+        public void setContinent(String continent) {
+            Continent = continent;
+        }
+
+        public String getRegion() {
+            return Region;
+        }
+
+        public void setRegion(String region) {
+            Region = region;
+        }
+
+        public String getPopulation() {
+            return Population;
+        }
+
+        public void setPopulation(String population) {
+            Population = population;
+        }
+
+        public String getCapital() {
+            return Capital;
+        }
+
+        public void setCapital(String capital) {
+            Capital = capital;
+        }
+
+
+
+
+
+    }
+
